7607958c902c11bf9498af0a13b42b8a46402c1a
NabilBettaieb <nabil.bettaieb@esprit.tn>
2022-03-07 03:30:31 +0100

    reponse

--- src/controller/ReclamationController.java
+++ src/controller/ReclamationController.java
@@ -5,14 +5,18 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import services.serviceReclamation;
 import model.Reclamation;
 
@@ -61,7 +65,20 @@ public class ReclamationController implements Initializable {
     }    
 
     @FXML
-    private void RepondreClicked(ActionEvent event) {
+    private void RepondreClicked(ActionEvent event) throws IOException {
+        ReponseRecController cont = new ReponseRecController();
+        cont.id_reponse = R.getIdrec();
+        cont.rec = R;
+        
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ReponseRec.fxml"));
+        loader.setController(cont);
+//        mainAnchor = loader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("My New Stage Title");
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
     }
 
     @FXML
