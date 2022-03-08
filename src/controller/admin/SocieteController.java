7bac87d112c29ecfaeaf4d1ceeffb69e7561f1a8
NabilBettaieb <nabil.bettaieb@esprit.tn>
2022-03-08 04:24:50 +0100

    Update SocieteController.java

--- src/controller/admin/SocieteController.java
+++ src/controller/admin/SocieteController.java
@@ -5,14 +5,18 @@
  */
 package controller.admin;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.societe;
 import services.serviceSociete;
 /**
@@ -54,7 +58,19 @@ serviceSociete ssoc = new serviceSociete();
     }    
 
     @FXML
-    private void ModifierClicked(ActionEvent event) {
+    private void ModifierClicked(ActionEvent event) throws IOException {
+        SocieteController cont = new SocieteController();
+        cont.Soc = this.Soc;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/controller/admin/societe.fxml"));
+        loader.setController(cont);
+//        mainAnchor = loader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("My New Stage Title");
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    
     }
 
     @FXML
