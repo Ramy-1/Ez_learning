e461278bccbfac659168e08f9718eb8f8ec5da4f
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-03-09 22:05:44 +0100

    MILKAAAA <3

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -28,10 +28,10 @@ public class FinalProject extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-//      Parent root = FXMLLoader.load(getClass().getResource("/main/Main.fxml"));
+      Parent root = FXMLLoader.load(getClass().getResource("/main/Main.fxml"));
 //        Parent root = FXMLLoader.load(getClass().getResource("/controller/admin/Home.fxml"));
 
-        Parent root = FXMLLoader.load(getClass().getResource("/controller/admin/Home.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/controller/admin/Home.fxml"));
 
         Scene scene = new Scene(root);
 //        scene.setFill(Color.TRANSPARENT);
