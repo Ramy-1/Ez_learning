7fa3105655be7870c58a48c37caf240bc2e4127e
NabilBettaieb <nabil.bettaieb@esprit.tn>
2022-05-16 05:55:23 +0100

    sign in done

--- src/view/SignInController.java
+++ src/view/SignInController.java
@@ -96,191 +96,139 @@ public class SignInController implements Initializable {
             User u = new User();
 
             System.out.println((u.Login(username.getText(), password.getText())));
+
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            stage.close();
+
+            Stage stage2 = new Stage();
+            ServiceUser sU = new ServiceUser();
+
+            Parent root;
+            Scene scene;
             if (u.Login(username.getText(), password.getText())) {
 
-                Stage stage = (Stage) loginButton.getScene().getWindow();
-                stage.close();
-
-                Stage stage2 = new Stage();
-                ServiceUser sU = new ServiceUser();
-                u = sU.getByMail(username.getText());
-                Role r = u.getRole();
-//                r = Role.admin;
-                System.out.println("role = " + r);
-                Parent root;
-                Scene scene;
-                switch (r) {
-                    case admin:
 //                        root = FXMLLoader.load(getClass().getResource("/etudiant/dashboardEtudiant.fxml"));
-                        root = FXMLLoader.load(getClass().getResource("/controller/admin/Home.fxml"));
+                root = FXMLLoader.load(getClass().getResource("/controller/admin/Home.fxml"));
 
-                        scene = new Scene(root);
-                        // scene.setFill(Color.TRANSPARENT);
-                        stage2.setScene(scene);
-                        // stage2.initStyle(StageStyle.UNDECORATED);
-                        // stage2.initStyle(StageStyle.TRANSPARENT);
-                        stage2.setTitle("Admin Panel");
-                        // drag it here
-                        root.setOnMousePressed(events -> {
-                            x = events.getSceneX();
-                            y = events.getSceneY();
-                        });
-                        root.setOnMouseDragged(events -> {
+                scene = new Scene(root);
+                // scene.setFill(Color.TRANSPARENT);
+                stage2.setScene(scene);
+                // stage2.initStyle(StageStyle.UNDECORATED);
+                // stage2.initStyle(StageStyle.TRANSPARENT);
+                stage2.setTitle("Admin Panel");
+                // drag it here
+                root.setOnMousePressed(events -> {
+                    x = events.getSceneX();
+                    y = events.getSceneY();
+                });
+                root.setOnMouseDragged(events -> {
 
-                            stage2.setX(events.getScreenX() - x);
+                    stage2.setX(events.getScreenX() - x);
 
-                            stage2.setY(events.getScreenY() - y);
+                    stage2.setY(events.getScreenY() - y);
 
-                        });
-                        stage2.show();
-                        break;
-                    case societe:
-                        root = FXMLLoader.load(getClass().getResource("/controller/societe/Home.fxml"));
-                        scene = new Scene(root);
-                        // scene.setFill(Color.TRANSPARENT);
-                        stage2.setScene(scene);
-                        // stage2.initStyle(StageStyle.UNDECORATED);
-                        // stage2.initStyle(StageStyle.TRANSPARENT);
-                        stage2.setTitle("Admin Panel");
-                        // drag it here
-                        root.setOnMousePressed(events -> {
-                            x = events.getSceneX();
-                            y = events.getSceneY();
-                        });
-                        root.setOnMouseDragged(events -> {
-
-                            stage2.setX(events.getScreenX() - x);
-
-                            stage2.setY(events.getScreenY() - y);
-
-                        });
-                        stage2.show();
-                        break;
-
-                    case etudiant:
-//                        root = FXMLLoader.load(getClass().getResource("/controller/societe/Home.fxml"));
-                        root = FXMLLoader.load(getClass().getResource("/etudiant/dashboardEtudiant.fxml"));
-
-                        scene = new Scene(root);
-                        // scene.setFill(Color.TRANSPARENT);
-                        stage2.setScene(scene);
-                        // stage2.initStyle(StageStyle.UNDECORATED);
-                        // stage2.initStyle(StageStyle.TRANSPARENT);
-                        stage2.setTitle("Admin Panel");
-                        // drag it here
-                        root.setOnMousePressed(events -> {
-                            x = events.getSceneX();
-                            y = events.getSceneY();
-                        });
-                        root.setOnMouseDragged(events -> {
-
-                            stage2.setX(events.getScreenX() - x);
-
-                            stage2.setY(events.getScreenY() - y);
-
-                        });
-                        stage2.show();
-                        break;
-                    case universite:
-                        root = FXMLLoader.load(getClass().getResource("/controller/Universite/Home.fxml"));
-                        scene = new Scene(root);
-                        // scene.setFill(Color.TRANSPARENT);
-                        stage2.setScene(scene);
-                        // stage2.initStyle(StageStyle.UNDECORATED);
-                        // stage2.initStyle(StageStyle.TRANSPARENT);
-                        stage2.setTitle("Admin Panel");
-                        // drag it here
-                        root.setOnMousePressed(events -> {
-                            x = events.getSceneX();
-                            y = events.getSceneY();
-                        });
-                        root.setOnMouseDragged(events -> {
-
-                            stage2.setX(events.getScreenX() - x);
-
-                            stage2.setY(events.getScreenY() - y);
-
-                        });
-                        stage2.show();
-                        break;
-
-                    default:
-                        break;
-                }
+                });
+                stage2.show();
             } else {
-                AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Error",
-                        "login incorrect");
-            }
 
-            // String query = "select * from user WHERE email = ? and pwd = ?";
-            // try {
-            // if (rs.next()) {
-            // Stage stage = (Stage) loginButton.getScene().getWindow();
-            // stage.close();
-            // int colind = rs.findColumn("role");
-            // System.out.println(colind);
-            // Object role = rs.getObject(colind);
-            // System.out.println(role);
-            // Stage stage2 = new Stage();
-            // if (role.toString().equals("admin")) {
-            // Parent root =
-            // FXMLLoader.load(getClass().getResource("/controller/Home.fxml"));
-            // /*
-            // * FXMLLoader loader = new
-            // * FXMLLoader(getClass().getResource("/components/dashboard.fxml"));
-            // * BorderPane borderpane = (BorderPane) loader.load();
-            // * Node menu =
-            // * FXMLLoader.load(getClass().getResource("/components/admin.fxml"));
-            // *
-            // * borderpane.setLeft(menu);
-            // */
-            // Scene scene = new Scene(root);
-            // scene.setFill(Color.TRANSPARENT);
-            // stage2.setScene(scene);
-            // stage2.initStyle(StageStyle.UNDECORATED);
-            // stage2.initStyle(StageStyle.TRANSPARENT);
-            // stage2.setTitle("Admin Panel");
-            // // drag it here
-            // root.setOnMousePressed(events -> {
-            // x = events.getSceneX();
-            // y = events.getSceneY();
-            // });
-            // root.setOnMouseDragged(events -> {
-            // stage2.setX(events.getScreenX() - x);
-            // stage2.setY(events.getScreenY() - y);
-            // });
-            // stage2.show();
-            // } else if (role.toString().equals("student")) {
-            // Parent root =
-            // FXMLLoader.load(getClass().getResource("/etudiant/dashboardEtudiant.fxml"));
-            // Scene scene = new Scene(root);
-            // stage.setScene(scene);
-            // stage.setTitle("Student Panel");
-            // stage.show();
-            // } else if (role.toString().equals("recruter")) {
-            // } else if (role.toString().equals("university")) {
-            // } else {
-            // FXMLLoader loader = new
-            // FXMLLoader(getClass().getResource("/dashboard/dashboard.fxml"));
-            // BorderPane borderpane = (BorderPane) loader.load();
-            // Node menu = FXMLLoader.load(getClass().getResource("/Menu/formateur.fxml"));
-            // // MainPanelController mwc = loader.getController();
-            // borderpane.setLeft(menu);
-            // Scene scene = new Scene(borderpane);
-            // stage.setScene(scene);
-            // stage.setTitle("Admin Panel");
-            // stage.show();
-            // }
-            // // Parent root =
-            // // FXMLLoader.load(getClass().getResource("/view/MainPanelView.fxml"));
-            // } else {
-            // AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Error",
-            // "Verifier votre email et mot de passe.");
-            // username.requestFocus();
-            // }
-            // } catch (SQLException ex) {
-            // System.out.println(ex);
-            // }
+//                        root = FXMLLoader.load(getClass().getResource("/controller/societe/Home.fxml"));
+                root = FXMLLoader.load(getClass().getResource("/etudiant/dashboardEtudiant.fxml"));
+
+                scene = new Scene(root);
+                // scene.setFill(Color.TRANSPARENT);
+                stage2.setScene(scene);
+                // stage2.initStyle(StageStyle.UNDECORATED);
+                // stage2.initStyle(StageStyle.TRANSPARENT);
+                stage2.setTitle("Admin Panel");
+                // drag it here
+                root.setOnMousePressed(events -> {
+                    x = events.getSceneX();
+                    y = events.getSceneY();
+                });
+                root.setOnMouseDragged(events -> {
+
+                    stage2.setX(events.getScreenX() - x);
+
+                    stage2.setY(events.getScreenY() - y);
+
+                });
+                stage2.show();
+
+            }
+        } else {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Error",
+                    "login incorrect");
         }
+
+        // String query = "select * from user WHERE email = ? and pwd = ?";
+        // try {
+        // if (rs.next()) {
+        // Stage stage = (Stage) loginButton.getScene().getWindow();
+        // stage.close();
+        // int colind = rs.findColumn("role");
+        // System.out.println(colind);
+        // Object role = rs.getObject(colind);
+        // System.out.println(role);
+        // Stage stage2 = new Stage();
+        // if (role.toString().equals("admin")) {
+        // Parent root =
+        // FXMLLoader.load(getClass().getResource("/controller/Home.fxml"));
+        // /*
+        // * FXMLLoader loader = new
+        // * FXMLLoader(getClass().getResource("/components/dashboard.fxml"));
+        // * BorderPane borderpane = (BorderPane) loader.load();
+        // * Node menu =
+        // * FXMLLoader.load(getClass().getResource("/components/admin.fxml"));
+        // *
+        // * borderpane.setLeft(menu);
+        // */
+        // Scene scene = new Scene(root);
+        // scene.setFill(Color.TRANSPARENT);
+        // stage2.setScene(scene);
+        // stage2.initStyle(StageStyle.UNDECORATED);
+        // stage2.initStyle(StageStyle.TRANSPARENT);
+        // stage2.setTitle("Admin Panel");
+        // // drag it here
+        // root.setOnMousePressed(events -> {
+        // x = events.getSceneX();
+        // y = events.getSceneY();
+        // });
+        // root.setOnMouseDragged(events -> {
+        // stage2.setX(events.getScreenX() - x);
+        // stage2.setY(events.getScreenY() - y);
+        // });
+        // stage2.show();
+        // } else if (role.toString().equals("student")) {
+        // Parent root =
+        // FXMLLoader.load(getClass().getResource("/etudiant/dashboardEtudiant.fxml"));
+        // Scene scene = new Scene(root);
+        // stage.setScene(scene);
+        // stage.setTitle("Student Panel");
+        // stage.show();
+        // } else if (role.toString().equals("recruter")) {
+        // } else if (role.toString().equals("university")) {
+        // } else {
+        // FXMLLoader loader = new
+        // FXMLLoader(getClass().getResource("/dashboard/dashboard.fxml"));
+        // BorderPane borderpane = (BorderPane) loader.load();
+        // Node menu = FXMLLoader.load(getClass().getResource("/Menu/formateur.fxml"));
+        // // MainPanelController mwc = loader.getController();
+        // borderpane.setLeft(menu);
+        // Scene scene = new Scene(borderpane);
+        // stage.setScene(scene);
+        // stage.setTitle("Admin Panel");
+        // stage.show();
+        // }
+        // // Parent root =
+        // // FXMLLoader.load(getClass().getResource("/view/MainPanelView.fxml"));
+        // } else {
+        // AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Error",
+        // "Verifier votre email et mot de passe.");
+        // username.requestFocus();
+        // }
+        // } catch (SQLException ex) {
+        // System.out.println(ex);
+        // }
     }
 }
+
